a1a9ffd0be499f2bee846362425effcc55ac972b
Zacarías Samaniego <zacariassamaniego@gmail.com>
2018-10-13 15:52:18 -0300

    Se termina el maquetado del AMB de áreas

--- src/app/vistas/cursos/cursos.component.ts
+++ src/app/vistas/cursos/cursos.component.ts
@@ -15,6 +15,7 @@ export class CursosComponent implements OnInit, DoCheck {
   public cursos = [];
   public cursosCopia = [];
   public tipoCursos = [];
+  public areas = ["Area1", "area2", "area3", "area2", "area3", "area2", "area3"]
   busqueda;
 
 
